ecabdf475b9a5ecff67dbecfef23de4ee060bd44
gsnorberto <sn_gabriel@outlook.com>
2022-07-10 09:56:58 -0300

    Created all stylesheets and initial components

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,23 @@
 import React from 'react'
 
+import { Product, FooterBanner, HeroBanner } from '../components'
+
 const home = () => {
     return (
         <>
-            Banner
+            <HeroBanner />
 
-            <div>
+            <div className='products-heading'>
                 <h2>Mais vendidos</h2>
                 <p>Shampoos para diversos tipos de cabelo</p>
             </div>
 
             {/* Products for sale */}
-            <div>
+            <div className='products-container'>
                 {['Produto 1 ', 'Produto 2 ', 'Produto 3 '].map(product => product)}
             </div>
 
-            Rodapé
+            <FooterBanner />
         </>
     )
 }
