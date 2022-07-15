71c61941d05ff184bb9d6fd96bad8e1544bc7187
gsnorberto <sn_gabriel@outlook.com>
2022-07-15 16:18:47 -0300

    Ie created the Footer and NavBar components.

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import Head from 'next/head'
+import NavBar from './NavBar'
+import Footer from './Footer'
 
-const Layout = () => {
-  return (
-    <div>Layout</div>
-  )
+const Layout = ({ children }) => {
+    return (
+        <div>
+            <Head>
+                <title>Gouveia Cosméticos - Loja</title>
+            </Head>
+
+            <header>
+                <NavBar />
+            </header>
+
+            <main className="main-container">
+                {children}
+            </main>
+
+            <footer>
+                <Footer />
+            </footer>
+        </div>
+    )
 }
 
 export default Layout
